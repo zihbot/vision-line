9edb93826dfa7a2990f6041ac94d8e454595a7e3
Botond Zih <zihbot@gmail.com>
2021-06-30 18:09:42 +0200

    From yaml

--- core/functions/greyscale.py
+++ core/functions/greyscale.py
@@ -1,12 +1,19 @@
+from os import name
 import cv2
 import numpy as np
 from .base_function import BaseFunction
+import yaml
 
 class Greyscale(BaseFunction):
     def __init__(self) -> None:
         super().__init__()
-        self.name = 'greyscale'
-        self.is_visible = True
+        self.data = yaml.load(open('../functions/greyscale.yaml'))
+        self.name = self.data['name']
+        #self.data['run'] = 'global img\n' + self.data['run']
 
-    def run(self, img: np.ndarray) -> np.ndarray:
-        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    def run(self, img_loc: np.ndarray) -> np.ndarray:
+        print(self.data['run'])
+        global img
+        img = img_loc
+        exec(self.data['run'], globals(), globals())
+        return img
