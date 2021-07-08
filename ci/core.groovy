3a194ad155eb081ab8ca2a85ad361e1c62ce8f1b
Botond Zih <zihbot@gmail.com>
2021-07-08 22:13:16 +0200

    jenkins

--- ci/core.groovy
+++ ci/core.groovy
@@ -8,11 +8,11 @@ node {
   }
 
   stage('Cleanup') {
-    sh 'docker ps -f name=core:latest -q | xargs --no-run-if-empty docker container stop'
-    sh 'docker container ls -a -fname=core:latest -q | xargs -r docker container rm'
+    sh 'docker ps -f name=core -q | xargs --no-run-if-empty docker container stop'
+    sh 'docker container ls -a -fname=core -q | xargs --no-run-if-empty docker container rm'
   }
 
   stage('Deploy') {
-    sh 'docker run -d --name core -v "functions/":"/functions" -p 80:80 core:latest'
+    sh 'docker run -d --name core -v $(pwd)"/functions":"/functions" -p 80:80 core:latest'
   }
 }
