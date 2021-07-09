5d06e2c086620867a4bc923575c3d18abe9069a7
Botond Zih <zihbot@gmail.com>
2021-07-09 17:49:30 +0200

    init webapp

--- vision-line-webapp/src/App.tsx
+++ vision-line-webapp/src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="">
-        <p>
-          Hello World!
-        </p>
-    </div>
-  );
+class App extends React.Component {
+  handleClick() {
+    fetch('/function').then(data => data.json().then( data => {
+      console.log('DATA', data);
+    })).catch(err => {
+      console.log('ERROR', err);
+    });
+  }
+
+  render() {
+    return (
+      <div className="">
+          <p>
+            Hello World!
+          </p>
+          <button onClick={() => this.handleClick()}>Gomb</button>
+      </div>
+    );
+  }
 }
 
 export default App;
