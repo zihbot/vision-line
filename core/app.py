4581bd678deb9010ca7c9551bd2b29ab1107017c
Botond Zih <zihbot@gmail.com>
2021-07-11 11:28:28 +0200

    set root route

--- core/app.py
+++ core/app.py
@@ -6,43 +6,46 @@ from flask import jsonify
 from flask_cors import CORS
 import controllers.image
 
-from flask import Flask, request
+from flask import Flask, request, Blueprint
 app = Flask(__name__)
 cors = CORS(app)
-app.config['APPLICATION_ROOT'] = '/api/v1'
+
+root = Blueprint("root_bp", __name__)
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 FunctionFactory.init()
 
-@app.route('/')
+@root.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/createImage', methods=['POST'])
+@root.route('/createImage', methods=['POST'])
 def create_image():
     return str(controllers.image.post_create_image(request.get_json(force=True)))
 
-@app.route('/createImage/<line_id>/add/<position>', methods=['POST'])
+@root.route('/createImage/<line_id>/add/<position>', methods=['POST'])
 def create_image_add(line_id: str, position: str):
     return 'OK' if controllers.image.create_image_add(int(line_id), int(position), request.get_json(force=True)) else 'ERROR'
 
-@app.route('/createImage/<line_id>/delete/<position>', methods=['DELETE'])
+@root.route('/createImage/<line_id>/delete/<position>', methods=['DELETE'])
 def create_image_delete(line_id: str, position: str):
     return 'OK' if controllers.image.create_image_delete(int(line_id), int(position)) else 'ERROR'
 
-@app.route('/image/<line_id>', methods=['GET'])
+@root.route('/image/<line_id>', methods=['GET'])
 def get_image(line_id: str):
     bytes = controllers.image.get_image(int(line_id))
     buffer = io.BytesIO(bytes)
     buffer.seek(0)
     return send_file(buffer, attachment_filename='image.jpg', mimetype='image/jpeg')
 
-@app.route('/line/<line_id>', methods=['GET'])
+@root.route('/line/<line_id>', methods=['GET'])
 def get_line(line_id: str):
     return jsonify(controllers.image.get_line(int(line_id)))
 
-@app.route('/function/', methods=['GET'])
+@root.route('/function/', methods=['GET'])
 def get_functions():
     return jsonify(FunctionFactory.get_all_function_inputs())
+
+app.register_blueprint(root, url_prefix='/api/v1')
