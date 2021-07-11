eaa8e2bbb0accebc1cdfb27112170e0e83e962e6
Botond Zih <zihbot@gmail.com>
2021-07-11 16:26:49 +0200

    add threshold, reorder, partial image

--- core/app.py
+++ core/app.py
@@ -29,6 +29,14 @@ def create_image():
 def create_image_add(line_id: str, position: str):
     return 'OK' if controllers.image.create_image_add(int(line_id), int(position), request.get_json(force=True)) else 'ERROR'
 
+@root.route('/createImage/<line_id>/add/<position>', methods=['PUT'])
+def create_image_edit(line_id: str, position: str):
+    return 'OK' if controllers.image.create_image_edit(int(line_id), int(position), request.get_json(force=True)) else 'ERROR'
+
+@root.route('/createImage/<line_id>/reorder', methods=['POST'])
+def create_image_reorder(line_id: str):
+    return 'OK' if controllers.image.create_image_reorder(int(line_id), request.get_json(force=True)) else 'ERROR'
+
 @root.route('/createImage/<line_id>/delete/<position>', methods=['DELETE'])
 def create_image_delete(line_id: str, position: str):
     return 'OK' if controllers.image.create_image_delete(int(line_id), int(position)) else 'ERROR'
@@ -40,9 +48,20 @@ def get_image(line_id: str):
     buffer.seek(0)
     return send_file(buffer, attachment_filename='image.jpg', mimetype='image/jpeg')
 
+@root.route('/image/<line_id>/<last_node_id>', methods=['GET'])
+def get_image_until_last_node(line_id: str, last_node_id: str):
+    bytes = controllers.image.get_image(int(line_id), int(last_node_id))
+    buffer = io.BytesIO(bytes)
+    buffer.seek(0)
+    return send_file(buffer, attachment_filename='image.jpg', mimetype='image/jpeg')
+
 @root.route('/line/<line_id>', methods=['GET'])
 def get_line(line_id: str):
     return jsonify(controllers.image.get_line(int(line_id)))
+    
+@root.route('/line', methods=['GET'])
+def get_lines_node_numbers():
+    return jsonify(controllers.image.get_lines_node_numbers())
 
 @root.route('/function/', methods=['GET'])
 def get_functions():
--- core/controllers/image.py
+++ core/controllers/image.py
@@ -22,16 +22,32 @@ def create_image_add(line_id: int, position: int, data: dict) -> int:
     lines[line_id].insert(position, data)
     return True
     
+def create_image_edit(line_id: int, position: int, data: dict) -> int:
+    logger.debug('create_image_edit data=%s', data)
+    lines[line_id][position] = data
+    return True
+    
+def create_image_reorder(line_id: int, data: list[int]) -> int:
+    logger.debug('create_image_reorder data=%s', data)
+    if len(data) != len(lines[line_id]):
+        return False
+    lines[line_id] = [lines[line_id][i] for i in data]
+    return True
+    
 def create_image_delete(line_id: int, position: int) -> int:
     logger.debug('patch_create_image line_id=%s, position=%s', line_id, position)
     lines[line_id].pop(position)
     return True
 
-def get_image(line_id: int) -> bytes:
-    logger.debug('get_image id=%s data=%s', line_id, lines[line_id])
+def get_image(line_id: int, last_node_id: int = None) -> bytes:
+    logger.debug('get_image id=%s last_node=%s data=%s', line_id, last_node_id, lines[line_id])
+    use_nodes = lines[line_id]
+    if last_node_id is not None:
+        use_nodes = use_nodes[:last_node_id]
+
     img_loc = cv2.imread('img.jpg')
 
-    for node in lines[line_id]:
+    for node in use_nodes:
         func = FunctionFactory.get_function(node['name'])
         func.set_inputs(node['inputs'] if 'inputs' in node else {})
         img_loc = func.run(img_loc)
@@ -41,3 +57,6 @@ def get_image(line_id: int) -> bytes:
 
 def get_line(line_id: int) -> list[dict]:
     return lines[line_id]
+
+def get_lines_node_numbers() -> list[dict]:
+    return [len(line) for line in lines]
