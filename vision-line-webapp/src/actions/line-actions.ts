6d2bf87f32cd5c191e8c0dd51c74e3d830a2a4a0
Botond Zih <zihbot@gmail.com>
2021-08-14 14:52:36 +0200

    usage of api

--- vision-line-webapp/src/actions/line-actions.ts
+++ vision-line-webapp/src/actions/line-actions.ts
@@ -1,16 +1,16 @@
 import { ThunkDispatch } from "redux-thunk";
 import { AnyAction } from "redux";
 import { RootState } from '../reducers/root-reducer';
-import { getLineNodes } from '../services/data-service';
 import { LineAction } from "../reducers/line-reducer";
+import api from '../services/data-service';
 
 export function loadLine(lineId: number) {
   return (dispatch: ThunkDispatch<RootState, undefined, AnyAction>, getState: () => RootState) => {
-    getLineNodes(lineId).then(data => {
+    api().linesLineIdNodesGet({lineId}).subscribe({next: data => {
       dispatch({ type: LineAction.SET_ALL_NODES, payload: data });
       console.log('STATE', getState());
-    }).catch(error => {
-
-    });
+    }, error: error => {
+      console.log('ERROR', error);
+    }});
   }
 }
