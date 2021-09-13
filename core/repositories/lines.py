6133e0e70fe9e69cb1d3d5e90e229880611c35da
Botond Zih <zihbot@gmail.com>
2021-09-13 08:27:11 +0200

    Adding db functions

--- core/repositories/lines.py
+++ core/repositories/lines.py
@@ -1,3 +1,4 @@
+from sqlalchemy.sql.expression import delete, select
 from sqlalchemy.sql.schema import Column
 from sqlalchemy.sql.sqltypes import JSON, BigInteger, Integer, String
 from .database import *
@@ -8,3 +9,17 @@ class LineORM(Base):
     name = Column(String(50))
     last_change = Column(BigInteger)
     nodes = Column(JSON)
+
+    def insert(self):
+        self.id = None
+        db_session.add(self)
+        db_session.commit()
+        return self.find_by_id(self.id)
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+    @classmethod
+    def find_by_id(cls, id: int) -> 'LineORM':
+        return db_session.execute(select(LineORM).where(LineORM.id == id)).scalars().first()
